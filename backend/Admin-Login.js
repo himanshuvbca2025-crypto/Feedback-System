35f6bb217c1540c4f49700a201d7708a73b90736
Himanshu Verma <himanshuv.bca2025@ssism.org>
2026-08-19 13:32:29 +0530

    feat: integrate dotenv for environment configuration and add new iteg page component

--- backend/Admin-Login.js
+++ backend/Admin-Login.js
@@ -1,69 +1,73 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors"
 import { MongoClient } from "mongodb";
 import bcrybt from "bcrypt";
+
+
+dotenv.config();
 
 const System = express();
 
 System.use(cors());
 System.use(express.json());
 
-const client=new MongoClient("mongodb://127.0.0.1:27017");
+const client = new MongoClient("mongodb://127.0.0.1:27017");
 await client.connect();
 
-const db=client.db("FeedBack_System");
-const cl=db.collection("Admin");
+const db = client.db("FeedBack_System");
+const cl = db.collection("Admin");
 
 
-System.post("/admin/login",async(req,res)=>{
+System.post("/admin/login", async (req, res) => {
 
- try {
-            
-    const {gmail,password}=req.body;
+    try {
 
-     if (!gmail || !password) {
+        const { gmail, password } = req.body;
+
+        if (!gmail || !password) {
             return res.status(400).json({
                 message: "Gmail and Password are required"
             });
         }
-        
-
-    const result=await cl.findOne({
-        gmail:gmail,
-    });
 
 
-    if(!result){
-       return res.status(401).json({
-            message:"Invalid Gmail or Password"
+        const result = await cl.findOne({
+            gmail: gmail,
+        });
+
+
+        if (!result) {
+            return res.status(401).json({
+                message: "Invalid Gmail or Password"
+            });
+        }
+        const isMatch = await bcrybt.compare(password, result.password);
+
+        if (isMatch) {
+            res.status(200).json({
+                message: "Login Successfully"
+            });
+        }
+        else {
+            res.status(401).json({
+                message: "Invalid gmail or password"
+            });
+        }
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: "Internal Server Error!!!"
         });
     }
-    const isMatch=await bcrybt.compare(password,result.password);
 
-    if(isMatch){
-        res.status(200).json({
-            message:"Login Successfully"
-        });
-    }
-    else{
-        res.status(401).json({
-            message:"Invalid gmail or password"
-        });
-    }
-
- } catch (error) {
-         
-         res.status(500).json({
-            message:"Internal Server Error!!!"
-         });
-    }
-    
 
 })
 
-System.listen(1000,()=>{
+System.listen(1000, () => {
     console.log("Server Running on 1000 port.....");
-    
+
 });
 
 
---
+++ frontend/src/pages/iteg.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+function Iteg() {
+
+    return (
+
+        <div className="iteg-page"></div>
+    )
+
+
+}
